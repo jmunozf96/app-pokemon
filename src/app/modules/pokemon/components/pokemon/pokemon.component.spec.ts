7c3659c7131efa599f556dd2081392f1460b3e8b
Jesus David Muñoz Farfan <jdmunozf97@gmail.com>
2022-08-26 10:23:43 -0500

    feat[test]: listado de pokemon busqueda

--- src/app/modules/pokemon/components/pokemon/pokemon.component.spec.ts
+++ src/app/modules/pokemon/components/pokemon/pokemon.component.spec.ts
@@ -3,6 +3,7 @@ import {PokemonComponent} from './pokemon.component';
 import {PokemonAccionService} from "../../services/pokemon-accion.service";
 import {HttpClientTestingModule} from "@angular/common/http/testing";
 import {NO_ERRORS_SCHEMA} from "@angular/core";
+import {uuiv4} from "../../../../core/helpers/functions.helper";
 
 describe('PokemonComponent', () => {
   let component: PokemonComponent;
@@ -32,5 +33,34 @@ describe('PokemonComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should filter data pokemon', function () {
+    fixture = TestBed.createComponent(PokemonComponent);
+    component = fixture.componentInstance;
+    component.dataSoruce$.next([{
+      name: 'Prueba',
+      image: '',
+      id: 1,
+      attack: 50,
+      defense: 50,
+      type: 'Desconocido',
+      hp: 100,
+      uuid: 'uuid',
+      idAuthor: 1
+    }]);
+    component.searchData('pru');
+    fixture.detectChanges();
+    expect(component.dataSoruce$.value).toEqual([{
+      name: 'Prueba',
+      image: '',
+      id: 1,
+      attack: 50,
+      defense: 50,
+      type: 'Desconocido',
+      hp: 100,
+      uuid: 'uuid',
+      idAuthor: 1
+    }]);
   });
 });
